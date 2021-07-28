c7b9486906d6a7c12a7ee39ca9fa54a01aa6301d
Tom Arrow <davorfilms@googlemail.com>
2021-07-28 17:13:05 +0200

    Add project files.

---
+++ DotFix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace DotFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] files = Directory.GetFiles(Environment.CurrentDirectory);
+            Console.WriteLine(files.Length);
+            foreach(string file in files)
+            {
+                if (file[file.Length-1] == '.')
+                {
+                    Console.WriteLine(file);
+                    File.Move(@"\\?\"+file, @"\\?\" + file.Substring(0, file.Length - 1));
+                    //break;
+                }
+            }
+            string[] dirs = Directory.GetDirectories(Environment.CurrentDirectory);
+            Console.WriteLine(dirs.Length);
+            foreach(string dir in dirs)
+            {
+                if (dir[dir.Length-1] == '.')
+                {
+                    Console.WriteLine(dir);
+                    Directory.Move(@"\\?\"+dir, @"\\?\" + dir.Substring(0, dir.Length - 1));
+                    //break;
+                }
+            }
+            if (Console.IsInputRedirected)
+            {
+
+                Console.WriteLine("Done. Press Enter to continue.");
+                Console.Read();
+            } else
+            {
+                Console.WriteLine("Done. Press key to continue.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
